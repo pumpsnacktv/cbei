b1b4e24cfd6174243fc508f4518ae5d124e28ddf
HosekiTheGlaceon <156066211+pumpsnacktv@users.noreply.github.com>
2024-05-12 00:23:10 +0000

    just-

--- e.py
+++ e.py
@@ -1,12 +1,12 @@
 import sys
 
 n = int(input())
-arr = list(map(int, sys.stdin.readline.split()))
+arr = list(map(int, sys.stdin.readline().split()))
 rarr = [0 for i in range(sum(arr)+1)]
 
 def d(i, s):
-    global num, arr
-    if i == num:
+    global n, arr
+    if i == n:
         if s > 0:
             rarr[s] = 1
         return
