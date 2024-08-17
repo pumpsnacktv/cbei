1a44a4cdaa2b735feacc345e30e051118e6e298a
sodakorei <156066211+pumpsnacktv@users.noreply.github.com>
2024-08-17 02:43:06 +0000

    미1쿠!

--- 8/nah.py
+++ 8/nah.py
@@ -1,3 +1,27 @@
-a = 0
-a = int(input())
-print(a)
+def checker(password):
+    mo, ja = 0, 0
+    for c in password:
+        if c in "aeiou":
+            mo += 1
+        else:
+            ja += 1
+    return mo >= 1 and ja >= 2
+
+
+def recu(n, alpha, password, i):
+    if len(password)==n:
+        if checker(password):
+            print(password)
+        return
+    if len(alpha)<=i:
+        return
+    
+    recu(n, alpha, password+alpha[i], i+1)
+    recu(n, alpha, password, i+1)
+
+
+l, c = map(int, input().split())
+s = input().split()
+s = sorted(s)
+
+recu(l, s, "", 0)
